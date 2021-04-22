a3b6eebdb375709e27d5f38dd15c7378521789c4
loknath998 <55695886+loknath998@users.noreply.github.com>
2021-04-22 07:05:57 +0530

    removed loading

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,26 @@
-import React, { useContext } from 'react';
-import {RowContext} from '../context';
-import Loading  from '../components/Loading';
+import React from 'react';
+// import { useContext } from 'react'
+// import {RowContext} from '../context';
+// import Loading  from '../components/Loading';
 
 import TabBar from '../components/TabBar';
 
 
 export default function Home({rows}){
-    const {loading} = useContext(RowContext);
+    // const {loading} = useContext(RowContext);
     // let [rows] =useState([]);
     // console.log(props);
     // console.log('sheet id',process.env.REACT_APP_SHEET_ID);
     // console.log('email',process.env.REACT_APP_CLIENT_EMAIL);
     // console.log("private key",process.env.REACT_APP_PRIVATE_KEY);
+    // return <>
+    //     <div>
+    //     {loading? <Loading />:<TabBar />}
+    //     </div>
+    // </>;
     return <>
         <div>
-        {loading? <Loading />:<TabBar />}
+        <TabBar />
         </div>
     </>;
 }
